3fb0a17fde5bcb8bba740e5b75583152cc4e83c3
AustinBenSimiyu <rubberducktoo@gmail.com>
2024-10-04 09:24:44 +0300

    Third attempt

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,14 @@ def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle of size n."""
     if n <= 0:
         return []
-    
     triangle = [[1]]  # Start with the first row
-    
     for i in range(1, n):
         prev_row = triangle[-1]
-        # Generate the next row by adding adjacent elements from the previous row
+        # Generate the next row by
+        # adding adjacent elements from the previous row
         row = [1]  # First element is always 1
         for j in range(1, i):
             row.append(prev_row[j-1] + prev_row[j])
         row.append(1)  # Last element is always 1
         triangle.append(row)
-    
     return triangle
