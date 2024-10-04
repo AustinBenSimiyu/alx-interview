de3902872403dda23d762a190d28bdf28fc8deca
AustinBenSimiyu <rubberducktoo@gmail.com>
2024-10-04 09:32:31 +0300

    Fifth attempt

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module contains a function to generate Pascal's triangle.
+
+The function `pascal_triangle(n)` returns a list of lists representing 
+the Pascal's triangle of size `n`.
+"""
 def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle of size n."""
     if n <= 0:
